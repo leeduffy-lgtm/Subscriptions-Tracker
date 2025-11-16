29ebcc0d8b4830edb1ab359cd261cf7d7bc60450
leeduffy-lgtm <lee.duffy@theyaks.uk>
2025-11-16 13:23:34 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,53 @@
-const CACHE_NAME = "subscriptions-tracker-cache-v1";
-const ASSETS_TO_CACHE = [
+// Name of the cache
+const CACHE_NAME = "subtracker-cache-v1";
+
+// Files you want to cache
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./home.html",
   "./manifest.json",
-  "./service-worker.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
+// Install – caches required files
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  // Activate immediately without waiting
+  self.skipWaiting();
 });
 
+// Activate – clears old caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      )
-    )
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
+  // Take control of all clients immediately
+  self.clients.claim();
 });
 
+// Fetch – network first, fallback to cache
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request).catch(() => cachedResponse);
-    })
+    fetch(event.request)
+      .then((response) => {
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
